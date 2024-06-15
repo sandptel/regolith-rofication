30e05080c7d53c57762b23501fa5d367f14d9b3b
sandptel <sandeeppatel626735@gmail.com>
2024-06-16 05:09:35 +0530

    added flake.nix

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.python311.pkgs.buildPythonApplication {
+  pname = "rofication";
+  version = "main";
+  
+  src = ./.;
+  
+ propagatedBuildInputs = with pkgs.python311.pkgs; [dbus-python pygobject3];
+  installPhase= ''
+  mkdir -p $out/bin
+  cp -r ./  $out/bin
+  '';
+  meta = {
+    mainProgram = "rofication";
+    description = " Notification system that provides a Rofi front-end ";
+    homepage = "https://github.com/regolith-linux/regolith-rofication";
+    license = pkgs.lib.licenses.gpl3Plus;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Regolith-rofication";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+  system= "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    packages.${system}.default= import ./default.nix{inherit pkgs;};
+     devShells.${system}.default = pkgs.mkShell{
+      nativeBuildInputs =[
+        (import ./default.nix{inherit pkgs;})
+      ];
+     };
+  };
+}
